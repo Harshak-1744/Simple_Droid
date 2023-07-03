0fc52b8a328c1d249c9372ecb4514baeff963458
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-07-03 21:04:12 +0530

    Update Droid.java

--- Droid.java
+++ Droid.java
@@ -1,49 +1,80 @@
 import java.util.*;
 import java.util.ArrayList;
 
-public class Droid
-{
+public class Droid {
     String welcome;
-    Droid(String value)
-    {
+    int battery;
+
+    Droid(String value) {
         this.welcome = value;
+        this.battery = 100;
     }
 
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         Droid M = new Droid("Hola...! I am Droid");
         System.out.println(M.welcome);
 
-        System.out.println("Tasks i can perform");
-
-
-        ArrayList<String> tasks = new ArrayList<>();
-        tasks.add("[1] Write a Mail and send it");
-        tasks.add("[2] Get the stock updated");
-        tasks.add("[3] Water the Plants");
-        tasks.add("[4] Task Remainder");
-
-        for(String i: tasks)
-        {
-            System.out.println(i);
-        }
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your task number:-");
-        int n = sc.nextInt();
-        
-        
-        switch(n)
-        {
-            case 1:
-                System.out.println("why bro");
-            break;
-        
-        }
-    
-    
+        String choice;
 
-    sc.close();    
+        do {
+            System.out.println("Tasks I can perform");
+
+            ArrayList<String> tasks = new ArrayList<>();
+            tasks.add("[1] Write a Mail and send it");
+            tasks.add("[2] Get the stock updated");
+            tasks.add("[3] Water the Plants");
+            tasks.add("[4] Task Reminder");
+
+            for (String i : tasks) {
+                System.out.println(i);
+            }
+
+            System.out.println("Enter your task number:");
+            int n = sc.nextInt();
+
+            switch (n) {
+                case 1:
+                    M.sendEmail();
+                    M.decreaseBattery();
+                    break;
+                case 2:
+                    System.out.println("Performing task: Get the stock updated");
+                    M.decreaseBattery();
+                    break;
+                case 3:
+                    System.out.println("Performing task: Water the Plants");
+                    M.decreaseBattery();
+                    break;
+                case 4:
+                    System.out.println("Performing task: Task Reminder");
+                    M.decreaseBattery();
+                    break;
+                default:
+                    System.out.println("Invalid task number");
+                    break;
+            }
+
+            System.out.println("Battery percentage: " + M.battery + "%");
+
+            System.out.println("Do you want to perform another task? (yes/no):");
+            choice = sc.next();
+        } while (choice.equalsIgnoreCase("yes"));
+
+        System.out.println("Exiting...");
+        sc.close();
     }
 
+    public void sendEmail() {
+        // Email sending logic
+        System.out.println("Performing task: Write a Mail and send it");
+    }
+
+    public void decreaseBattery() {
+        if (battery >= 4) {
+            battery -= 4;
+        } else {
+            battery = 0;
+        }
+    }
 }
